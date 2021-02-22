d6d6f2669b8793a8b8b9476cdd1dbe057d8a3592
Steven Harris <stevenharrisdev@gmail.com>
2021-02-22 10:43:50 -0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+      ],
+    },
+  },
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

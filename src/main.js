364fb5561010e7b820ee6fcf0a60d3caae26c3f3
Steven Harris <stevenharrisdev@gmail.com>
2021-02-22 11:29:23 -0700

    add font awesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import '@/assets/css/tailwind.css';
 
+import { FontAwesomeIcon } from '@/plugins/font-awesome';
 import { createApp } from 'vue';
 
 import App from './App.vue';
 
-createApp(App).mount("#app");
+createApp(App).component("fa", FontAwesomeIcon).mount("#app");
